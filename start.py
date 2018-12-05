3f09f31598af5806ac3194f9d7f055305ebf65ff
Prateek Gupta <prateekgupta.3991@gmail.com>
2018-12-05 16:05:22 +0530

    updating values to real valued functions

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 import sklearn
-features = [[140, "smooth"], [160, "smooth"], [150, "bumpy"], [180, "bumpy"]]
-labels = ["apple", "apple", "orange", "orange"]
+features = [[140, 0], [160, 0], [150, 1], [180, 1]] # updating to real valued functions [0-smooth,1-bumpy]
+labels = [0, 0, 1, 1] # updating to real valued functions [0-apple, 1-orange]
 print (features, labels)
