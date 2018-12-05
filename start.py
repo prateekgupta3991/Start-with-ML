d40f4cc414ce61131aab8bf7cbefdf1e1a6dac7f
Prateek Gupta <prateekgupta.3991@gmail.com>
2018-12-05 16:07:25 +0530

    added decision tree classifier

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-import sklearn
+from sklearn import tree
 features = [[140, 0], [160, 0], [150, 1], [180, 1]] # updating to real valued functions [0-smooth,1-bumpy]
 labels = [0, 0, 1, 1] # updating to real valued functions [0-apple, 1-orange]
 print (features, labels)
+
+classifier = tree.DecisionTreeClassifier()
