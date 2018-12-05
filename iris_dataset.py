07d56800d1b845da9caa263be5e903740028384d
Prateek Gupta <prateekgupta.3991@gmail.com>
2018-12-05 18:07:05 +0530

    print iris dataset features etc

--- iris_dataset.py
+++ iris_dataset.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import load_iris
 
-iris = load_iris()
-print (iris)
+iris_dataset = load_iris()
+print (iris_dataset.feature_names)
+print (iris_dataset.target_names)
+print (iris_dataset.data[10])
