69373a6569fd9293e70e88cf5de0ff3961dc99d7
Prateek Gupta <prateekgupta.3991@gmail.com>
2018-12-05 18:16:25 +0530

    import dataset and use various fields done

--- iris_dataset.py
+++ iris_dataset.py
@@ -4,3 +4,7 @@ iris_dataset = load_iris()
 print (iris_dataset.feature_names)
 print (iris_dataset.target_names)
 print (iris_dataset.data[10])
+print (iris_dataset.target[2])
+
+for i in range(len(iris_dataset.target)):
+    print ("Example %d : label %s, features %s" % (i, iris_dataset.target[i], iris_dataset.data[i]))
